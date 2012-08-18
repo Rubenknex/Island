dfcdf8cbcd0f3fbb39fe41fee0307733467d967f
Ruben van Gulik <rubenvangulik@gmail.com>
2012-08-18 16:37:20 +0200

    Added some comments

--- perlinnoise.lua
+++ perlinnoise.lua
@@ -1,4 +1,4 @@
-require "functions"
+require "utils"
 
 Perlin2D = {}
 Perlin2D.__index = Perlin2D
@@ -11,6 +11,7 @@ function Perlin2D.create(width, height, persistance, octaves)
 	self.persistance = persistance
 	self.octaves = octaves
 
+	-- Create a map filled with random noise.
 	self.noise = {}
 	for x=1, width do
 		self.noise[x] = {}
@@ -22,11 +23,13 @@ function Perlin2D.create(width, height, persistance, octaves)
 	return self
 end
 
-function Perlin2D:smoothNoise(octave)
-	local smooth = {}
+function Perlin2D:generateOctave(octave)
 	local period = 2.0 ^ octave
 	local frequency = 1.0 / period
 
+	-- Retrieve the surrounding values for each value in the noise map and calculate
+	-- an interpolated value using cosine interpolation.
+	local smooth = {}
 	for x=0, self.width - 1 do
 		smooth[x + 1] = {}
 
@@ -39,45 +42,49 @@ function Perlin2D:smoothNoise(octave)
 			local bottom = (top + period) % self.height
 			local verBlend = (y - top) * frequency
 
-			local above = math.cerp(self.noise[left + 1][top + 1], self.noise[right + 1][top + 1], horBlend)
-			local below = math.cerp(self.noise[left + 1][bottom + 1], self.noise[right + 1][bottom + 1], horBlend)
+			local above = utils.cerp(self.noise[left + 1][top + 1], self.noise[right + 1][top + 1], horBlend)
+			local below = utils.cerp(self.noise[left + 1][bottom + 1], self.noise[right + 1][bottom + 1], horBlend)
 
-			smooth[x + 1][y + 1] = math.cerp(above, below, verBlend)
+			smooth[x + 1][y + 1] = utils.cerp(above, below, verBlend)
 		end
 	end
 
 	return smooth
 end
 
-function Perlin2D:perlinNoise()
-	local smooth = {}
+function Perlin2D:generate()
+	local layers = {}
 
+	-- Generate a smooth noise map for each octave.
 	for i=1, self.octaves do
-		smooth[i] = self:smoothNoise(i - 1)
+		layers[i] = self:generateOctave(i - 1)
 	end
 
+	-- Fill the perlin noise map with zeroes.
 	local perlin = {}
 	for x=1, self.width do
 		perlin[x] = {}
 		for y=1, self.height do
-			perlin[x][y] = 0.0
+			perlin[x][y] = 0
 		end
 	end
 
 	local amplitude = 1.0
 	local totalAmplitude = 0.0
 
+	-- Add all the values from the octaves multiplied by the amplitude together.
 	for i=self.octaves, 1, -1 do
 		amplitude = amplitude * self.persistance
 		totalAmplitude = totalAmplitude + amplitude
 
 		for x=1, self.width do
 			for y=1, self.height do
-				perlin[x][y] = perlin[x][y] + smooth[i][x][y] * amplitude
+				perlin[x][y] = perlin[x][y] + layers[i][x][y] * amplitude
 			end
 		end
 	end
 
+	-- Devide all the values by the total amplitude to put it in range [0-1].
 	for x=1, self.width do
 		for y=1, self.height do
 			perlin[x][y] = perlin[x][y] / totalAmplitude
