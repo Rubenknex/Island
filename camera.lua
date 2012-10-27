cc0c1ebccb0b69067426fac17b578758fe8a9841
Ruben van Gulik <rubenvangulik@gmail.com>
2012-10-27 18:46:16 +0200

    Fixed artifacts due to floats as camera coords

--- camera.lua
+++ camera.lua
@@ -9,20 +9,29 @@ function Camera.create(x, y, width, height)
 
     self.x, self.y = x, y
     self.width, self.height = width, height
+    self:roundPosition()
 
     return self
 end
 
 function Camera:setPosition(pos)
     self.x, self.y = pos.x, pos.y
+    self:roundPosition()
 end
 
 function Camera:interpolate(pos, var)
     self.x = utils.lerp(self.x, pos.x, var)
     self.y = utils.lerp(self.y, pos.y, var)
+    self:roundPosition()
 end
 
 function Camera:move(offset)
     self.x = self.x + offset.x
     self.y = self.y + offset.y
+    self:roundPosition()
+end
+
+function Camera:roundPosition()
+    self.x = math.floor(self.x + 0.5)
+    self.y = math.floor(self.y + 0.5)
 end
