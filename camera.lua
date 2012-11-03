614e40d0c43de53aef2de834563f37bb0b5d5f21
Ruben van Gulik <rubenvangulik@gmail.com>
2012-11-03 16:43:22 +0100

    Camera is not global anymore

--- camera.lua
+++ camera.lua
@@ -3,16 +3,25 @@ require "utils"
 
 -- Credit goes to: http://nova-fusion.com/2011/04/19/cameras-in-love2d-part-1-the-basics/
 
-camera = {
-    width = love.graphics.getWidth(),
-    height = love.graphics.getHeight(),
-    x = 0,
-    y = 0,
-    scale = 1,
-    rotation = 0,
-}
+Camera = {}
+Camera.__index = Camera
 
-function camera:set()
+function Camera.create(x, y, scale, rotation)
+    local self = {}
+    setmetatable(self, Camera)
+
+    self.width = love.graphics.getWidth()
+    self.height = love.graphics.getHeight()
+
+    self.x = x or 0
+    self.y = y or 0
+    self.scale = scale or 1
+    self.rotation = rotation or 0
+
+    return self
+end
+
+function Camera:set()
     love.graphics.push()
     
     love.graphics.translate(self.width / 2, self.height / 2)
@@ -21,37 +30,37 @@ function camera:set()
     love.graphics.translate(-self.x, -self.y)
 end
 
-function camera:unset()
+function Camera:unset()
     love.graphics.pop()
 end
 
-function camera:move(dx, dy)
-    self.x = self.x + dx
-    self.y = self.y + dy
-end
-
-function camera:lerp(x, y, a)
+function Camera:lerp(x, y, a)
     self.x = self.x + (x - self.x) * a
     self.y = self.y + (y - self.y) * a
 end
 
-function camera:rotate(dr)
+function Camera:move(dx, dy)
+    self.x = self.x + dx
+    self.y = self.y + dy
+end
+
+function Camera:rotate(dr)
     self.rotation = self.rotation + dr
 end
 
-function camera:setPosition(x, y)
+function Camera:setPosition(x, y)
     self.x = x
     self.y = y
 end
 
-function camera:setScale(scale)
+function Camera:setScale(scale)
     self.scale = scale
 end
 
-function camera:setRotation(rotation)
+function Camera:setRotation(rotation)
     self.rotation = rotation
 end
 
-function camera:getBounds()
+function Camera:getBounds()
     return Rect.create(self.x - self.width / 2, self.y - self.height / 2, self.width, self.height)
 end
