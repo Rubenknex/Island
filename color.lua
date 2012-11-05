1a0b4400d3a3caf878e49b6eebc915d59f0fed2f
Ruben van Gulik <rubenvangulik@gmail.com>
2012-11-05 08:22:07 +0100

    Color.interpolate is now static

--- color.lua
+++ color.lua
@@ -15,32 +15,33 @@ function Color.fromHSL(h, s, l)
     setmetatable(self, Color)
 
     self.r, self.g, self.b = Color.hslToRgb(h, s, l)
-    print(tostring(self))
 
     return self
 end
 
 function Color.hslToRgb(h, s, l)
-    h, s, l = (h/360)*6, s/100, l/100
+    h, s, l = (h / 360) * 6, s / 100, l / 100
 
-    local c = (1-math.abs(2*l-1))*s
-    local x = (1-math.abs(h%2-1))*c
-    local m,r,g,b = (l-.5*c), 0,0,0
-    if h < 1     then r,g,b = c,x,0
-    elseif h < 2 then r,g,b = x,c,0
-    elseif h < 3 then r,g,b = 0,c,x
-    elseif h < 4 then r,g,b = 0,x,c
-    elseif h < 5 then r,g,b = x,0,c
-    else              r,g,b = c,0,x
+    local c = (1 - math.abs(2 * l - 1)) * s
+    local x = (1 - math.abs(h % 2 - 1)) * c
+
+    local m, r, g, b = (l - .5 * c), 0, 0, 0 
+
+    if     h < 1 then r, g, b = c, x, 0
+    elseif h < 2 then r, g, b = x, c, 0
+    elseif h < 3 then r, g, b = 0, c, x
+    elseif h < 4 then r, g, b = 0, x, c
+    elseif h < 5 then r, g, b = x, 0, c
+    else              r, g, b = c, 0, x
     end 
 
-    return (r+m)*255,(g+m)*255,(b+m)*255,a
+    return (r + m) * 255, (g + m) * 255, (b + m) * 255, a
 end
 
-function Color:interpolate(other, x)
-    return Color.fromRGB(utils.lerp(self.r, other.r, x), 
-                         utils.lerp(self.g, other.g, x), 
-                         utils.lerp(self.b, other.b, x))
+function Color.interpolate(a, b, x)
+    return Color.fromRGB(utils.lerp(a.r, b.r, x), 
+                         utils.lerp(a.g, b.g, x), 
+                         utils.lerp(a.b, b.b, x))
 end
 
 function Color:toRGB()
